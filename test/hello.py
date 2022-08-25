c75117fcb1c92ca9db188cdbefc33e42207fa452
Sriniketh J <srinikethcr7@gmail.com>
2022-08-25 23:57:37 +0530

    updated: ref code

--- test/hello.py
+++ test/hello.py
@@ -1,7 +1,11 @@
-def hello(name = "World"):
+def hello(name = "World"):  
+    
+    '''
+    Simple hello function
+    Parameters:
+    - name (str) : any string
+    Returns:
+    - a string
+    '''
 
     return "Hello " + str(name)
-
-import pydaisi as pyd
-print_hello = pyd.Daisi(username/"Print Hello")
-
